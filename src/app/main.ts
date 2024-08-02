c3d833d17156d8a73d29c7260012e2c83247dcce
Victor Osipov <victorosipovprogrammer@gmail.com>
2024-08-02 13:39:22 +0300

    add logs and base /admin

--- src/app/main.ts
+++ src/app/main.ts
@@ -6,3 +6,8 @@ import { createApp } from 'vue'
 const app = createApp(App)
 registerPlugins(app)
 app.mount('#app')
+
+console.log('BASE_URL', import.meta.env.BASE_URL)
+console.log('url', import.meta.url)
+console.log('PROD', import.meta.env.PROD)
+console.log('DEV', import.meta.env.DEV)
--- vite.config.mts
+++ vite.config.mts
@@ -49,4 +49,5 @@ export default defineConfig({
     server: {
         port: 3000,
     },
+    base: '/admin'
 })
