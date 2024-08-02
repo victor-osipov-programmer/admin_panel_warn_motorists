7b00919a73660f9ab781b2b8c8b79f011aca4730
Victor Osipov <victorosipovprogrammer@gmail.com>
2024-08-02 12:50:26 +0300

    add import.meta.env.BASE_URL

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -3,7 +3,7 @@ import { routes } from "./routes";
 import { useLocalStorage } from "@vueuse/core";
 
 export const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes
 })
 
