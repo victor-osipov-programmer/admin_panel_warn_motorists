e49637274326e13a76ac06c5c89f97e4582d24bf
Victor Osipov <victorosipovprogrammer@gmail.com>
2024-08-04 09:07:17 +0300

    redesign mailing

--- src/widgets/app-mailing/ui/app-mailing.vue
+++ src/widgets/app-mailing/ui/app-mailing.vue
@@ -1,28 +1,34 @@
 <template>
     <div class="app-mailing pa-5">
-        <div class="pl-10 py-5">
-            <FloatLabel>
-                <InputText id="title" v-model="title" />
-                <label for="title">Заголовок</label>
-            </FloatLabel>
-        </div>
+        <Card class="card">
+            <template #content>
+                <div class="pl-10 py-5">
+                    <FloatLabel>
+                        <InputText id="title" v-model="title" />
+                        <label for="title">Заголовок</label>
+                    </FloatLabel>
+                </div>
 
-        <v-file-input v-model="file" accept="image/*" class="file" label="Фото"></v-file-input>
+                <v-file-input v-model="file" accept="image/*" class="file" label="Фото"></v-file-input>
 
-        <v-textarea class="message mb-3" v-model="message" label="Сообщение" prepend-icon="mdi-comment" rows="4"
-            auto-grow></v-textarea>
+                <v-textarea class="message mb-3" v-model="message" label="Сообщение" prepend-icon="mdi-comment" rows="4"
+                    auto-grow></v-textarea>
 
-        <div class="ml-10 d-flex ga-3">
-            <slot name="actions"></slot>
+                <div class="ml-10 d-flex ga-3">
+                    <slot name="actions"></slot>
 
-            <v-btn @click="emit('confirmMailing', title, message, file)" prepend-icon="mdi-check-circle">
-                <template v-slot:prepend>
-                    <v-icon color="success"></v-icon>
-                </template>
+                    <v-btn @click="emit('confirmMailing', title, message, file)" prepend-icon="mdi-check-circle">
+                        <template v-slot:prepend>
+                            <v-icon color="success"></v-icon>
+                        </template>
 
-                <slot></slot>
-            </v-btn>
-        </div>
+                        <slot></slot>
+                    </v-btn>
+                </div>
+            </template>
+
+        </Card>
+
     </div>
 </template>
 
@@ -39,8 +45,15 @@ const file = ref<null | File>(null)
 </script>
 
 <style lang="scss" scoped>
-.message,
-.file {
-    max-width: 600px
+.app-mailing {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100dvh;
+}
+
+.card {
+    width: 40vw;
 }
 </style>
